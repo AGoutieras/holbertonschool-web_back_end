9b79d9359f247bd788b873fe7f0d7f250699f609
AGoutieras <goutieras.anthony@gmail.com>
2026-04-07 11:25:37 +0200

    add 1-concurrent_coroutines.py

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    calls = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for coroutine in asyncio.as_completed(calls):
+        delay = await coroutine
+        delays.append(delay)
+    return delays
