2f0d362325a0b68537effd28bcb4dfa03f5a4bb2
AGoutieras <goutieras.anthony@gmail.com>
2026-04-07 14:45:49 +0200

    add 100-safe_first_element.py

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,9 @@
+# The types of the elements of the input are not known
+from typing import Sequence, Any, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
