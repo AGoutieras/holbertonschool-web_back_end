e48da06596b95272fb2862f0b9467271ce42b15d
AGoutieras <goutieras.anthony@gmail.com>
2026-04-09 09:26:28 +0200

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ Yield random numbers asynchronously at one-second intervals.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Yield 10 random floats between 0 and 10.
     """
