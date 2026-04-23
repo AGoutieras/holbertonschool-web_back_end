5f32f03ddf8a6a8da9add1ac4253fcf859e1d766
AGoutieras <goutieras.anthony@gmail.com>
2026-04-23 10:36:53 +0200

    fix !!

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print(f"\tmethod {method}: {count}")
 
-    status = collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    status = collection.count_documents({
+        'method': 'GET',
+        'path': '/status'
+    })
     print(f"{status} status check")
