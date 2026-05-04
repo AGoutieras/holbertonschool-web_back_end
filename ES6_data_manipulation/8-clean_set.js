25131d4c6d811e97f95deb0da1c5b166942f9101
AGoutieras <goutieras.anthony@gmail.com>
2026-05-04 10:36:33 +0200

    fix: improve input validation in cleanSet function to ensure startString is a string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet (set, startString) {
   const values = Array.from(set);
-  if (!startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
   return values.filter(val => val.startsWith(startString)).map(val => val.slice(startString.length)).join('-');
