80cbc122c81fb793175a0b46e8c2af0a46f6e43e
AGoutieras <goutieras.anthony@gmail.com>
2026-05-11 01:26:54 +0200

    feat: implement countStudents function to read student data from a file

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8')
+    const students = data.split('\n').filter(line => line).slice(1)
+    console.log(`Number of students: ${students.length}`)
+
+    const fields = {}
+    students.forEach(line => {
+      const field = line.split(',')[3]
+      const student = line.split(',')[0]
+      if (!fields[field]) {
+        fields[field] = []
+      }
+      fields[field].push(student)
+    })
+    for (const key in fields) {
+      console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`)
+    }
+  }
+
+  catch (err) {
+    throw new Error('Cannot load the database')
+  }
+}
+
+module.exports = countStudents
