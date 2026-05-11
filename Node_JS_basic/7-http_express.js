6f322b9a1cb82e400bd1ce423351eb3b06fa56db
AGoutieras <goutieras.anthony@gmail.com>
2026-05-11 12:04:20 +0200

    feat: implement HTTP server with student data processing and greeting response

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const database = process.argv[2]
+const express = require('express')
+const app = express()
+const port = 1245
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'))
+      } else {
+        const students = data.split('\n').filter(line => line).slice(1)
+        let result = `Number of students: ${students.length}\n`
+
+        const fields = {}
+        students.forEach(line => {
+          const field = line.split(',')[3]
+          const student = line.split(',')[0]
+          if (!fields[field]) {
+            fields[field] = []
+          }
+          fields[field].push(student)
+        })
+        for (const key in fields) {
+          result += `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}\n`
+        }
+        resolve(result)
+      }
+    })
+  })
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!')
+})
+app.get('/students', (req, res) => {
+  countStudents(database)
+  .then((result) => {
+  res.send(`This is the list of our students\n${result}`)
+  })
+})
+
+app.listen(1245, 'localhost')
+
+module.exports = app
