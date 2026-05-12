c8b0c2c5d6328ea985e3511aaf8a96c42cc2a66e
AGoutieras <goutieras.anthony@gmail.com>
2026-05-12 11:14:56 +0200

    feat: add StudentsController with methods to retrieve all students and students by major

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,36 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents (req, res) {
+    res.status(200);
+    readDatabase(process.argv[2])
+      .then((fields) => {
+        let result = 'This is the list of our students\n';
+        Object.keys(fields).sort().forEach(key => {
+          result += `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}\n`;
+        });
+        res.send(result);
+      })
+      .catch((error) => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor (req, res) {
+    res.status(200);
+
+    if (req.params.major !== 'CS' && req.params.major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      readDatabase(process.argv[2])
+        .then((fields) => {
+          res.send(`List: ${fields[req.params.major].join(', ')}`)
+        })
+        .catch((error) => {
+          res.status(500).send('Cannot load the database')
+        })
+    }
+  }
+}
+
+export default StudentsController
