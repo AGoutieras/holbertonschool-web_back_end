2b061cae7ec9ca3623d617b16fd158d20ffd9b70
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 01:36:46 +0200

    fix: remove unnecessary blank line in 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,7 +4,6 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
