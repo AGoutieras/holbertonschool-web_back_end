6f510f99a96423d3352c14637f19da40b300a06b
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 01:20:13 +0200

    fix: improve code formatting and remove redundant function definition in 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,46 +1,20 @@
-const fs = require('fs')
-const database = process.argv[2]
-const express = require('express')
-const app = express()
-const port = 1245
+const express = require('express');
+const countStudents = require('./3-read_file_async');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'))
-      } else {
-        const students = data.split('\n').filter(line => line).slice(1)
-        let result = `Number of students: ${students.length}\n`
-
-        const fields = {}
-        students.forEach(line => {
-          const field = line.split(',')[3]
-          const student = line.split(',')[0]
-          if (!fields[field]) {
-            fields[field] = []
-          }
-          fields[field].push(student)
-        })
-        for (const key in fields) {
-          result += `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}\n`
-        }
-        resolve(result)
-      }
-    })
-  })
-}
+const database = process.argv[2];
+const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 app.get('/students', (req, res) => {
   countStudents(database)
-  .then((result) => {
-  res.send(`This is the list of our students\n${result}`)
-  })
-})
+    .then((result) => {
+      res.send(`This is the list of our students\n${result}`);
+    });
+});
 
-app.listen(1245, 'localhost')
+app.listen(port, 'localhost');
 
-module.exports = app
+module.exports = app;
