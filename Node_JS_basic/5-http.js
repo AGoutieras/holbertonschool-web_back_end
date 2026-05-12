92442353d1832daa6ba78adfeba10a0bf9e81198
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 01:35:46 +0200

    fix: improve response header handling in HTTP server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,16 +4,18 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
+  
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(database)
       .then((result) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(`This is the list of our students\n${result}`);
       })
       .catch(() => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Cannot load the database');
       });
   }
