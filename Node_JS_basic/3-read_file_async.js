6520012881d9cd12e278496566e0e54293a80ff6
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 00:47:51 +0200

    fix: add missing semicolons for code consistency in 3-read_file_async.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'))
+        reject(new Error('Cannot load the database'));
       } else {
-        const students = data.split('\n').filter(line => line).slice(1)
-        console.log(`Number of students: ${students.length}`)
+        const students = data.split('\n').filter((line) => line).slice(1);
+        console.log(`Number of students: ${students.length}`);
 
-        const fields = {}
-        students.forEach(line => {
-          const field = line.split(',')[3]
-          const student = line.split(',')[0]
+        const fields = {};
+        students.forEach((line) => {
+          const field = line.split(',')[3];
+          const student = line.split(',')[0];
           if (!fields[field]) {
-            fields[field] = []
+            fields[field] = [];
           }
-          fields[field].push(student)
-        })
-        for (const key in fields) {
-          console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`)
-        }
-        resolve()
+          fields[field].push(student);
+        });
+        Object.keys(fields).forEach((key) => {
+          console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+        });
+        resolve();
       }
-    })
-  })
+    });
+  });
 }
 
-module.exports = countStudents
+module.exports = countStudents;
