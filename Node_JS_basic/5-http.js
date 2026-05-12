1414b24103e4943336ced73b7e292d592d72bfa7
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 01:18:52 +0200

    fix: improve code formatting by consolidating response header definition

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,9 +4,7 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain',
-  });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
