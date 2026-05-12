f462d1a892dc61cc060d9f0cb60ce651c8671d98
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 01:57:35 +0200

    fix: implement countStudents function to read and process student data from file

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,9 +1,36 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const database = process.argv[2];
 const app = express();
 const port = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const students = data.split('\n').filter((line) => line).slice(1);
+        let result = `Number of students: ${students.length}\n`;
+
+        const fields = {};
+        students.forEach((line) => {
+          const field = line.split(',')[3];
+          const student = line.split(',')[0];
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(student);
+        });
+        Object.keys(fields).forEach((key) => {
+          result += `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}\n`;
+        });
+        resolve(result.trimEnd());
+      }
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
