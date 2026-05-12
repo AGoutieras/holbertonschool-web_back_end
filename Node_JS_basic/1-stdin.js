b19eec65bf99c658afb1211f6b432894010eba3d
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 00:39:08 +0200

    refactor: replace readline interface with direct stdin handling for simplicity

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-
-  input: process.stdin,
-  output: process.stdout,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (input) => {
-  if (input) {
-    process.stdout.write(`Your name is: ${input}\n`);
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
