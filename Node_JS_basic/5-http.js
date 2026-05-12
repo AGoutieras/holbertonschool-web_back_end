551592d4d348bb6602f07011ad6c4f679b22aad7
AGoutieras <goutieras.anthony@gmail.com>
2026-05-13 01:44:15 +0200

    fix: implement countStudents function to read and process student data from file

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,7 +1,34 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const database = process.argv[2];
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const students = data.split('\n').filter((line) => line).slice(1);
+        let result = `Number of students: ${students.length}\n`;
+
+        const fields = {};
+        students.forEach((line) => {
+          const field = line.split(',')[3];
+          const student = line.split(',')[0];
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(student);
+        });
+        Object.keys(fields).forEach((key) => {
+          result += `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}\n`;
+        });
+        resolve(result.trimEnd());
+      }
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
